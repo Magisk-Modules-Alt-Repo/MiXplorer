e9b43e4acab0a0a216717d95034c9d81d856b3d5
Psk-Ita <pasquale.pinto@gmail.com>
2022-02-14 08:02:08 +0100

    installatio and user app for easy update process

---
+++ customize.sh
@@ -0,0 +1,22 @@
+#!/system/bin/sh
+
+pm install "$MODPATH/system/priv-app/com.mixplorer.tagger/com.mixplorer.tagger.apk" > /dev/null 2>&1
+
+pm install "$MODPATH/system/priv-app/com.mixplorer.smb/com.mixplorer.smb.apk" > /dev/null 2>&1
+
+pm install "$MODPATH/system/priv-app/com.mixplorer.signer/com.mixplorer.signer.apk" > /dev/null 2>&1
+
+pm install "$MODPATH/system/priv-app/com.mixplorer.pdf/com.mixplorer.pdf.apk" > /dev/null 2>&1
+
+pm install "$MODPATH/system/priv-app/com.mixplorer.metadata/com.mixplorer.metadata.apk" > /dev/null 2>&1
+
+pm install "$MODPATH/system/priv-app/com.mixplorer.image/com.mixplorer.image.apk" > /dev/null 2>&1
+
+pm install "$MODPATH/system/priv-app/com.mixplorer.codecs/com.mixplorer.codecs.apk" > /dev/null 2>&1
+
+pm install "$MODPATH/system/priv-app/com.mixplorer.autotag/com.mixplorer.autotag.apk" > /dev/null 2>&1
+
+pm install "$MODPATH/system/priv-app/com.mixplorer.archive/com.mixplorer.archive.apk" > /dev/null 2>&1
+
+pm install "$MODPATH/system/priv-app/com.mixplorer/com.mixplorer.apk" > /dev/null 2>&1
+
